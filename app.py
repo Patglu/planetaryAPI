1c47f2596fd530201a0deafca689e809e3a593aa
patglu <pat@glucode.com>
2023-10-12 20:32:19 +0200

    planets are created and can be accessed from route

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 import os
+from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ def default_hi():
 
 # Configure our database 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 @app.cli.command('db_create')
 def db_create():
@@ -32,39 +34,38 @@ def fb_drop():
 
 @app.cli.command('db_seed')
 def db_seed():
-    mercury = Planet(planet_name = 'Mercury',
-                                     planet_type = 'Class D',
-                                     home_star='Sol',
-                                     mass=3.258e23,
-                                     radius=1516,
-                                     distance=35.98e6)
-    
-    venus = Planet(planet_name = 'Venus',
-                                     planet_type = 'Class k',
-                                     home_star='Sol',
-                                     mass=4.867e24,
-                                     radius=3760,
-                                     distance=67.24e6)
-    
-    earth = Planet(planet_name = 'Earth',
-                                     planet_type = 'Class M',
-                                     home_star='Sol',
-                                     mass=5.972e24,
-                                     radius=3959,
-                                     distance=92.96e6)
-    
+    mercury = Planet(planet_name='Mercury',
+                     planet_type='Class D',
+                     home_star='Sol',
+                     mass=2.258e23,
+                     radius=1516,
+                     distance=35.98e6)
+
+    venus = Planet(planet_name='Venus',
+                         planet_type='Class K',
+                         home_star='Sol',
+                         mass=4.867e24,
+                         radius=3760,
+                         distance=67.24e6)
+
+    earth = Planet(planet_name='Earth',
+                     planet_type='Class M',
+                     home_star='Sol',
+                     mass=5.972e24,
+                     radius=3959,
+                     distance=92.96e6)
+
     db.session.add(mercury)
     db.session.add(venus)
     db.session.add(earth)
 
     test_user = User(first_name='William',
-                                     last_name='Herschel',
-                                     email='test@test.com',
-                                     password='P@ssw0rd')
-    
+                     last_name='Herschel',
+                     email='test@test.com',
+                     password='P@ssw0rd')
+
     db.session.add(test_user)
-    # Commit needed to save session
-    db.session.commit() 
+    db.session.commit()
     print('Database seeded!')
 
 @app.route('/<name>')
@@ -102,13 +103,15 @@ def parameters():
         return jsonify(message= "We meet again " + name)
     
 
+@app.route('/planets', methods= ['GET'])
+def planets():
+    planets_list = Planet.query.all()
+    result = planets_schema.dump(planets_list)
+    return jsonify(result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
-
+# database models
 class User(db.Model):
-    # Define all the fields 
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
@@ -121,9 +124,30 @@ class Planet(db.Model):
     __tablename__ = 'planets'
     planet_id = Column(Integer, primary_key=True)
     planet_name = Column(String)
-    planet_type = Column(String)    
+    planet_type = Column(String)
     home_star = Column(String)
     mass = Column(Float)
     radius = Column(Float)
     distance = Column(Float)
 
+
+# tell marshmallow which fields its looking for
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+
+
+class PlanetSchema(ma.Schema):
+    class Meta: 
+        fields = ('planet_id', 'planet_name', 'planet_type',
+                  'home_star', 'mass', 'radius', 'distance')
+        
+user_schema = UserSchema()
+# deserliaze multple records back 
+users_schema = UserSchema(many=True)
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
