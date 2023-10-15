cc7ed3f748ac4d7dcf4e717a9bae48c31d23f440
patglu <pat@glucode.com>
2023-10-15 15:19:21 +0200

    Planets can be added
    The API now allows you to add planets

--- app.py
+++ app.py
@@ -183,6 +183,29 @@ def planet_details(planet_id: int):
     else:
         return jsonify(message="That planet deos not exsist"), 404
 
+@app.route('/add_planet', methods=['POST'])
+def add_planet():
+    planet_name = request.args.get('planet_name')
+    test = Planet.query.filter_by(planet_name=planet_name).first()
+    if test:
+        return jsonify("There is already a planet by that name"), 409
+    else:
+        planet_type = request.args.get('planet_type')
+        home_star = request.args.get('home_star')
+        mass = float(request.args.get('mass'))
+        radius = float(request.args.get('radius'))
+        distance = float(request.args.get('distance'))
+        
+        new_planet = Planet(planet_name=planet_name,
+                            planet_type=planet_type,
+                            home_star=home_star,
+                            mass=mass,
+                            radius=radius,
+                            distance=distance)
+        
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify(message="You added a planet"), 201
 
 # database models
 class User(db.Model):
